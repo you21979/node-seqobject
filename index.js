c5649642ac56b0eed8bf1557a4a8115d25149281
Yuki Akiyama <you2197901@gmail.com>
2015-12-03 02:38:26 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict"
+var SeqObject = module.exports = function(obj){
+    Object.call(this, obj || {});
+};
+SeqObject.prototype.max = function(){
+    var self = this;
+    return Object.keys(self)
+        .reduce(function(r,key){ return Math.max(self[key], r) }, 0)
+}
+SeqObject.prototype.min = function(){
+    var self = this;
+    return Object.keys(self)
+        .reduce(function(r,key){ return Math.min(self[key], r) }, 0)
+}
+SeqObject.prototype.sizeOf = function(){
+    return this.max() + 1
+}
+SeqObject.prototype.toArray = function(){
+    var self = this;
+    return Object.keys(self)
+        .map(function(key){ return self[key] })
+}
+SeqObject.create = function(list){
+    var spec = list.reduce(function(r, v, idx){
+        r[v] = idx;
+        return r;
+    }, new seqObject())
+    return Object.freeze(spec);
+}
